7ac1788f0f65c073e27fa4ea77febc1834411e5c
JosueTario <00092625@uca.edu.sv>
2025-05-20 12:43:08 -0600

    Add files via upload

---
+++ For.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n,suma;
+
+    cout<<"Ingrese un numero";
+    cin>>n;
+
+        for (int i = 2; i < n ; i++)
+        {
+            suma=i+2;
+
+            cout<<suma<<endl;
+        }
+        
+
+
+
+
+    return 0;
+}
